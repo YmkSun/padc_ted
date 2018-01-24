4c981895a081295e89a4c58140b2060a2cd6bc85
Ye Myo Kyaw <ymksun90@gmail.com>
2018-01-25 01:46:47 +0630

    Skeleton Structure

---
+++ app/src/main/java/com/ymksun/dev/ted/TEDApp.java
@@ -0,0 +1,18 @@
+package com.ymksun.dev.ted;
+
+import android.app.Application;
+
+/**
+ * Created by yemyokyaw on 1/24/18.
+ */
+
+public class TEDApp extends Application {
+
+    public static final String APP_TAG = "TEDApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/com/ymksun/dev/ted/activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ymksun.dev.ted.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ymksun.dev.ted.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.left_menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
